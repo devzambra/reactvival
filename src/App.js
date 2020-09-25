3b343f6accecdb2f089d69a63e9e7b4d9443392e
Manuel <manuel.zambrano@grupoica.com>
2020-09-25 13:52:35 +0200

    feat (#noTask): Add tailwind. Add Header, Footer and Stats components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import Header from './components/ui/header/Header';
+import Footer from './components/ui/footer/Footer';
+import Stats from './components/stats/Stats';
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App flex flex-col min-h-screen'>
+			<Header />
+			<div className='main flex flex-1 flex-col items-center'>
+				<main className='container'>
+					<Stats />
+				</main>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/stats/Stats.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const stats = () => (
+	<div className='my-4 flex justify-between h-16'>
+		<span>Health</span>
+		<span>Energy</span>
+		<span>Hunger</span>
+		<span>Thirst</span>
+		<span>Money</span>
+	</div>
+);
+
+export default stats;
